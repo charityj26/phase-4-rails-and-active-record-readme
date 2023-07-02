d788713a551203b2df605a6902383ccebb404369
charityj26 <charity.joseph@student.moringaschool.com>
2023-07-02 17:17:30 +0300

    completed assignment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_25_143508) do
+ActiveRecord::Schema.define(version: 2023_07_02_143508) do
 
     create_table "cheeses", force: :cascade do |t|
       t.string "name"
